ce941a2b0ac1f7dd1d582c167868014ebd84624b
FlamingMSF <43052300+FlamingMSF@users.noreply.github.com>
2025-11-23 01:22:33 -0500

    Attempt to fix not leveling up bug

--- index.js
+++ index.js
@@ -311,6 +311,7 @@ async function handleXpGain(message) {
     let requiredXp = getRequiredXpForLevel(newLevel + 1);
 
     while (data.xp >= requiredXp && newLevel < 1000) {
+      xp -= requiredXp;
       newLevel++;
       requiredXp = getRequiredXpForLevel(newLevel + 1);
     }
@@ -2202,4 +2203,5 @@ client.on("interactionCreate", async (interaction) => {
 // BOT LOGIN
 // =====================================================
 client.login(process.env.Bot_Token);
+
 
